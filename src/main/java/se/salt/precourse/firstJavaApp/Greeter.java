8b698f0f380e8d3893209b50a0334e3af9eb1ec3
Jagoda Kosewska <jagoda@Shidfars-MacBook-Pro.local>
2022-07-13 18:32:53 +0200

    main

--- src/main/java/se/salt/precourse/firstJavaApp/Greeter.java
+++ src/main/java/se/salt/precourse/firstJavaApp/Greeter.java
@@ -3,11 +3,15 @@ package se.salt.precourse.firstJavaApp;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 public class Greeter {
-  private static String greet(String namePassedIn) {
-    return "Welcome to SALT, ";
-  }
+  private static String greet(String namePassedIn) {return "Welcome to SALT, " + namePassedIn;
+}
 
   public static void main(String[] args) throws IOException {
     System.out.print("What is your name? ");
@@ -15,8 +19,16 @@ public class Greeter {
     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
     String name = reader.readLine();
 
-    String greeting = greet(name);
+    System.out.print("When does the course start? ");
 
+    BufferedReader dateReader = new BufferedReader(new InputStreamReader(System.in));
+    String dateStr = dateReader.readLine();
+
+    LocalDate date = LocalDate.parse(dateStr);
+    LocalDate nowDate = LocalDate.now();
+
+    String greeting = greet(name);
     System.out.println(greeting);
+    System.out.println("Today is " + (nowDate.datesUntil(date).count()) + " days left until course starts");
   }
 }
